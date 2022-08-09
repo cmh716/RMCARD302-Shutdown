5e88d29dffbfa147ad8855b05a0da9db4d203fa7
Chris Hanson <chrishanson.me@icloud.com>
2022-08-09 11:27:01 -0400

    test changes

--- macos_install.sh
+++ macos_install.sh
@@ -21,7 +21,7 @@ XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     <string>com.cmh716.RMCARD302</string>
     <key>ProgramArguments</key>
     <array>
-        <string>/usr/bin/python3</string>
+        <string>/usr/local/bin/python3</string>
         <string>$BINDIR/$FILENAME</string>
         <string>-p $PORT</string>
     </array>
